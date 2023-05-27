2fa63c629e0ff6c7f2735acb6ddcc6aa7098a548
Sourav Ukil <Sourav@Sourav-Ukil.local>
2023-05-28 00:27:37 +0600

    done for today'

---
+++ js/27-may.js
@@ -0,0 +1,155 @@
+
+const authur = "Sourav";
+
+
+// premitive data types
+let oneNumber = 100;
+let twoString = "Sourav";
+let thereNull = null;
+let fourUndefined = undefined;
+let fifthBigInt = BigInt('123') + BigInt('7');
+let sixBoolean = true || false;
+let sevenSymble = Symbol("SR");
+
+
+// non premitive data types
+
+let theObjectOne = {
+  fName : authur,
+  lName : "Ukil",
+  age: 20
+};
+// console.log(theObjectOne);
+
+
+let theObjectTwo = {
+  sourav: "Hi",
+  joy: "hello sourav",
+  sourav: "ki obostha joy?",
+  joy: "bhalo, tor ?",
+  sourav: "valo"
+}
+
+// console.log(theObjectTwo);
+// console.log(theObjectTwo.joy);
+// console.log(theObjectTwo["joy"]);
+
+
+
+// assignment operetors
+
+// let a = 10,
+//     b = 5;
+
+// ++a;
+// console.log(a)
+// a++;
+// console.log(a)
+
+
+// a += 15;
+// a -= 5;
+// a *= 2;
+// // a %= 10;
+// a /= 2;
+// a **= 2; 
+// console.log(a)
+
+
+// comparison operetors
+
+// let a = 6,
+//     b = 5;
+
+// console.log(a == b);
+// console.log(a === b);
+// console.log(a != b);
+// console.log(a !== b);
+// console.log(a >= b);
+// console.log(a <= b);
+// console.log(a > b);
+// console.log(a < b);
+
+
+
+// logical operetors
+
+// let a = 6,
+//     b = 5;
+
+// console.log(a !== 5 && b <= a )
+// console.log((a === 5 && b >= a) || (a != b && b <= a))
+
+// console.log(!a);
+// console.log(!true);
+// console.log(!false);
+
+
+
+
+// conditional expressions
+// let getAge = Number.parseInt(prompt());
+// console.log(getAge)
+
+// if(getAge >= 18){
+//   alert("You can drive")
+// } else if (getAge != 18){
+//   alert("you can\'t drive")
+// }
+
+// ternery operetor
+
+// console.log("You can", (getAge>=18 ? "drive" : "not drive!"))
+
+
+
+
+
+// switch
+
+// let herAge = Number.parseInt(prompt());
+
+// switch (herAge) {
+//   case 17:
+//     console.log("You are 17 years old")
+//     break;
+//   case 18:
+//     console.log("You are 18 years old")
+//     break;
+//   case 19:
+//     console.log("You are 19 years old")
+//     break;
+
+//   default:
+//     console.log("Sorry only age 17-19 allowed")
+//     break;
+// }
+
+
+
+
+// for loop 
+// for(let i = 0; i < 10; i++){
+//   console.log(i+1);
+// } 
+
+
+// while loop
+
+// let i = 0;
+// while(i < 10){
+//   i++;
+//   console.log(i);
+// }
+
+// do{
+//   i++;
+//   console.log(i);
+// } while(i < 25)
+
+
+
+
+
+
+
