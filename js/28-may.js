0454e52618d788a41dcd0bec996596819d026d11
Sourav Ukil <Sourav@Sourav-Ukil.local>
2023-05-28 22:49:26 +0600

    all

---
+++ js/28-may.js
@@ -0,0 +1,93 @@
+
+// function
+function toMe(a, b){
+  // console.log("done")
+  return (10 * a) + (200 - b);
+};
+
+// console.log(toMe(10, 20));
+
+let x = 10,
+    y = 5,
+    z = 15;
+
+// console.log(toMe(x,y));
+// console.log(toMe(x,z));
+
+
+
+// array function
+const toMeTwo = ()=>{
+  console.log("working");
+};
+
+// toMeTwo();
+
+
+
+// getting marks from the object
+const toObject = {
+  akhi: 99,
+  riya: 97,
+  sourav: 50,
+  joy: 60,
+  asa: 33
+};
+
+// for(let i in toObject){
+//   console.log(toObject[i]);
+// }
+
+
+const authur = "Sourav";
+const her = "Rue";
+
+// console.log(authur.length);
+// console.log(authur[2]);
+
+
+
+// template literal
+let anDialogue = 
+  `${authur} is in love with ${her}
+  \n${her} is a good girl, ${her} loves to love ${authur}`
+;
+
+// console.log(anDialogue);
+
+// string methods 
+// console.log(authur.toLocaleUpperCase());
+// console.log(her.toLocaleLowerCase());
+
+// console.log(authur.slice(1,2));
+// console.log(authur.slice(1,4));
+// console.log(authur.slice(2));
+
+// console.log(anDialogue.replace("is", "yooo"));
+// console.log(anDialogue.replaceAll("is", "Niceeeee"));
+
+// console.log(authur.concat(" " + "is in love with" + " " + her))
+
+// let a = "                  baaa";
+let b = "hey there are you okay ?";
+// console.log(a)
+// console.log(a.trim())
+// console.log(b.trim("hey there"));
+
+
+
+// create string;
+let newString = new String("Sourav");
+// console.log(newString);
+
+
+// console.log(anDialogue.endsWith("meeeee"));
+// console.log(anDialogue.endsWith(authur));
+// console.log(anDialogue.startsWith("heeeeee"));
+// console.log(anDialogue.startsWith(authur));
+
+// return boolean 
+
+
+
+
