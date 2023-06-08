0eaf25b27201300922883375766c094892a2dc7a
Sourav <sourav@Souravs-MacBook-Air.local>
2023-06-08 23:43:38 +0600

    done

--- js/browser-events.js
+++ js/browser-events.js
@@ -1,1 +1,34 @@
 
+const authur = "Sourav";
+
+
+function click(){
+  console.log('hey')
+}
+
+let qrap = document.getElementsByClassName("qrap")[0];
+let box = document.getElementsByClassName("box")[0];
+// let box = document.createElement("div");
+
+// qrap.appendChild(box);
+// box.className = "box";
+
+function omOver() {
+  box.style.backgroundColor = "tomato"
+}
+
+function chngClr(){
+  box.style.backgroundColor = "teal"
+}
+
+function rotateBox(){
+  box.className = "rotate"
+  console.log("hello")
+}
+
+function rightClicks(){
+  box.style.backgroundColor = "#000"
+}
+
+
+
---
+++ js/string-methods.js
@@ -0,0 +1,43 @@
+
+const authur = "Sourav";
+const her = "rue";
+const anArray = [7 , 2, 27, 3, 18]
+
+let baal;
+
+baal = authur.toLocaleLowerCase()
+baal = authur.toLocaleUpperCase()
+baal = authur.replace("sourav", "ukil")
+baal = authur.replaceAll("sourav", "ukil")
+baal = authur.concat(her);
+baal = authur.charAt("2")
+baal = authur.charAt("0")
+baal = "          a".trim()
+baal = "          a".trimStart()
+baal = "          a".trimEnd()
+baal = anArray.slice(0, 2, 100, 200)
+baal = anArray.splice(0, 1, 360, 143)
+baal = anArray.length
+
+
+console.log(baal)
+
+
+// String replace()
+// String replaceAll()
+// String toUpperCase()
+// String toLowerCase()
+// String concat()
+// String charAt()
+// String charCodeAt()
+// String split()
+// String trim()
+// String trimStart()
+// String trimEnd()
+// String padStart()
+// String padEnd()
+// String substr()
+// String substring()
+// String slice()
+
+
