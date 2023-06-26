35072a351e0c71489afd18c512593b76ae9b7a05
Sourav <sourav@Souravs-MacBook-Air.local>
2023-06-27 01:43:03 +0600

    done

---
+++ js/arrayMethods.js
@@ -0,0 +1,64 @@
+const characters = [
+  {
+      name: 'Luke Skywalker',
+      height: '172',
+      mass: '77',
+      eye_color: 'blue',
+      gender: 'male',
+  },
+  {
+      name: 'Darth Vader',
+      height: '202',
+      mass: '136',
+      eye_color: 'yellow',
+      gender: 'male',
+  },
+  {
+      name: 'Leia Organa',
+      height: '150',
+      mass: '49',
+      eye_color: 'brown',
+      gender: 'female',
+  },
+  {
+      name: 'Anakin Skywalker',
+      height: '188',
+      mass: '84',
+      eye_color: 'blue',
+      gender: 'male',
+  },
+];
+
+const numbers = [2,5,67,3,78,2,5,76,87,4,3,34,5,73,63];
+
+
+// Array.filter()
+
+// const filter = characters.filter(character => character.mass < 100)
+// console.log(filter)
+// let genderMaleFeMalee = characters.filter(character => character.gender == 'male')
+// console.log(genderMaleFeMalee)
+// const eyeColor = characters.filter(x => x.eye_color = 'blue')
+// console.log(eyeColor)
+
+
+
+
+
+//  for each loop
+// const numbers = [2,5,67,3,78,2,5,76,87,4,3,34,5,73,63];
+
+// numbers.forEach((value, index, array)=>{
+  //     console.log(value, index, array)
+  // })
+  
+// let sum = 0;
+// numbers.forEach(value =>{
+//       return sum += value
+//   })
+  
+//   console.log(sum)
+
+
+
+// https://github.com/jamesqquick/javascript-array-functions-practice
