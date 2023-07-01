dca5b357e5d870e329a068c89f209b2d6a4c092b
Sourav <sourav@Souravs-MacBook-Air.local>
2023-07-01 23:43:12 +0600

    done

---
+++ js/Error Handling.js
@@ -0,0 +1,59 @@
+
+
+
+// setTimeout(()=>{
+//     console.log("hacking......")
+// }, 1000)
+
+// // console.log(sourav); // the error
+// try{
+//   console.log(sourav); // the error
+// }
+// catch(error){
+//    console.log("the error - ", error)
+// }
+
+// // try{
+// //   setTimeout(()=>{
+// //     try{
+// //       console.log(ukil); // the error
+// //     } catch(error){
+// //       console.log("the error - ", error)
+// //    }
+// //   }, 100)
+// // }
+
+
+// setTimeout(()=>{
+//     console.log("fetching.... please wait..")
+// }, 2000)
+
+// setTimeout(()=>{
+//     console.log("done...... in 3 seconds")
+// }, 3000)
+
+
+
+try {
+    console.log(sourav)
+    // sourav
+    // throw new Baal("error -505, sourav")
+    // throw new Error("error -505, sourav")
+    throw new ReferenceError("so much error -0909")
+}
+
+catch(error){
+    console.log(error)
+    console.log(iukil)
+    // console.log(error.name)
+    // console.log(error.message)
+    // console.log(error.stack)
+}
+
+finally{
+    // console.log(notDone)
+    console.log("done")
+}
+
+
+
