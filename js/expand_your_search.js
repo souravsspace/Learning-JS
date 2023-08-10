fd00275fd0266bf6905a6fb6dfb4512231406c62
Sourav <sourav@Souravs-MacBook-Air.local>
2023-08-11 01:49:45 +0600

    done

--- js/expand_your_search.js
+++ js/expand_your_search.js
@@ -1,27 +1,80 @@
-// search animation
-const searchBox = document.querySelector("[searchBox]")
+// ! variable 
+const search = document.querySelector("[search]")
 const inputBox = document.querySelector("[inputBox]")
 const searchBtn = document.querySelector("[searchBtn]")
 const closeBtn = document.querySelector("[closeBtn]")
 
+const cardTemplate = document.querySelector("[cardTemplate]")
+const cardList = document.querySelector("[cardList]")
 
-// search animation main logic
-searchBox.addEventListener("click", ()=>{
-    let val = searchBox.classList.contains("active")
-    if(!val){
-        searchBox.classList.add("active");
-        searchBox.focus();
-    } else {
-        searchBox.value = '';
-    }
-})
-
-closeBtn.addEventListener("click", ()=>{
-  console.log(searchBox.classList)
-  console.log(searchBox.classList.remove('active'))
-
-  searchBox.classList.remove('active')
-})
+let users = []
 
 
+
+// ! main logic
+const main = ()=>{
+
+      // ? search transition
+      
+      searchBtn.addEventListener('click', ()=>{
+            const toValue = search.classList.contains('active');
+
+            if(toValue){
+                inputBox.value = ''
+            } 
+            else {
+              search.classList.add('active')
+              inputBox.focus()
+            }
+      })
+
+      closeBtn.addEventListener('click', ()=> search.classList.remove('active'))
+      
+
+      // ? search bar logic
+
+
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => {
+          users = data.map(user => {
+            const card = cardTemplate.content.cloneNode(true).children[0]
+            const userName = card.querySelector("[userName]")
+            const userEmail = card.querySelector("[userEmail]")
+            const userUsername = card.querySelector("[userUsername]")
+
+            userName.textContent = user.name
+            userEmail.textContent = user.email
+            userUsername.textContent = "@" + user.username
+            
+            cardList.appendChild(card)
+
+            return { userName: user.name, userEmail: user.email, userUsername: user.username, element: card}
+          }) 
+      })
+
+      inputBox.addEventListener('input', (e)=>{
+        const value = e.target.value.toLowerCase()
+        
+        users.forEach(user =>{
+            const name = user.userName.toLowerCase().includes(value)
+            const username = user.userEmail.toLowerCase().includes(value)
+            const email = user.userUsername.toLowerCase().includes(value)
+
+            const isVisible = name || username || email
+
+            user.element.classList.toggle('hide', !isVisible)
+        })
+
+      })
+}
+
+main();
+    
+    
+    
+
+
+
+// https://codepen.io/amit-kharod/pen/xxaZyyW
 
